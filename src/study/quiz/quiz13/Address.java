9478c67aad7fa37c2cbde26df5d5d6d8cfcf410d
Yong-jin <takeardor@gmail.com>
2025-01-07 12:05:27 +0900

    api dao dto 개념 및 퀴즈

---
+++ src/study/quiz/quiz13/Address.java
@@ -0,0 +1,9 @@
+package study.quiz.quiz13;
+
+public class Address {
+	String street;
+	String city;
+	String zipcode;
+	
+	//생성자, getter setter
+}
---
+++ src/study/quiz/quiz13/Contact.java
@@ -0,0 +1,9 @@
+package study.quiz.quiz13;
+
+public class Contact {
+	
+	String type;
+	String value;
+	
+	//생성자, getter setter
+}
